83e055d9ceccfa3ceb002b7cb39b9ec2fb05e813
Dao Vu Khanh <126807347+KDaBest04@users.noreply.github.com>
2026-03-30 22:12:18 +0700

    Update load_to_db.py

--- load_to_db.py
+++ load_to_db.py
@@ -2,25 +2,19 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 1. ĐỌC DỮ LIỆU TỪ FILE CSV
 try:
     print("Đang đọc dữ liệu từ file laptop_data.csv...")
     df = pd.read_csv('laptop_data.csv')
     df = df.drop_duplicates(subset=['Ten', 'Ngay_cap_nhat'])
     print(f"Đã đọc thành công {len(df)} dòng dữ liệu.")
 except FileNotFoundError:
-    print("Lỗi: Không tìm thấy file laptop_data.csv. Bạn đã chạy file cào dữ liệu chưa?")
     exit()
 
-# 2. LẤY KẾT NỐI TỪ BIẾN MÔI TRƯỜNG (BẢO MẬT TUYỆT ĐỐI)
-# Thay vì viết thẳng mật khẩu ra đây, code sẽ tự đi tìm biến có tên 'DATABASE_URL'
 DB_URL = os.environ.get('DATABASE_URL')
 
 if not DB_URL:
     print("Lỗi: Không tìm thấy biến môi trường DATABASE_URL. Hãy kiểm tra lại cài đặt Secrets trên GitHub!")
     exit()
-
-# 3. ĐẨY LÊN CLOUD NEON
 print("Đang khởi động phi thuyền kết nối với máy chủ Neon (Singapore)...")
 try:
     engine = create_engine(DB_URL)
@@ -29,7 +23,6 @@ try:
 
     print("Success!")
 
-    # --- ĐOẠN CODE KIỂM TRA ---
     print("\nĐang kiểm tra lại tổng số lượng laptop trên mây...")
     check_df = pd.read_sql("SELECT COUNT(*) FROM laptop_prices_history", engine)
     tong_so_dong = check_df.iloc[0, 0]
